a6d58b3eb0869f352d3e71446ca41fa92f61859a
Claudio Condor <claucondor@gmail.com>
2025-08-08 20:15:47 -0400

    Change port to 8080

--- api/main.py
+++ api/main.py
@@ -382,7 +382,7 @@ app.mount("/", StaticFiles(directory="frontend/dist", html=True), name="static")
 
 def main():
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8080)
 
 if __name__ == "__main__":
     main()
